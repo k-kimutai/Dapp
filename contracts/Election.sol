4ae21f2c377e32bafe91d04db778d67f02506b2d
k-kimutai <kelvinkimutz21@gmail.com>
2018-08-20 07:09:37 +0300

    Initial Commit

---
+++ contracts/Election.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.17;
+
+contract Election {
+    
+    //add candidate info
+    //vote count info
+    //fetch candidate info
+//model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint votecount;
+    }
+    //store the candidates
+    mapping(uint => Candidate) public candidates;
+    //store candidates count
+    //use a counter cash to determine the numer of candidates in the mapping
+    uint public candidatesCount;
+    
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate (candidatesCount,_name, 0);
+    }
+    function Election () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");    
+    }
+}
